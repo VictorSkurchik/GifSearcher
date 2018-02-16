b84a9c6d78d62320637bcb59de2995e7609f4511
Victor <victorskurchik@gmail.com>
2018-02-16 10:45:44 +0300

    progress bar on gif_item added

--- app/src/main/java/com/victorskurchik/gifsearcher/viewmodel/ItemGifViewModel.java
+++ app/src/main/java/com/victorskurchik/gifsearcher/viewmodel/ItemGifViewModel.java
@@ -3,12 +3,18 @@ package com.victorskurchik.gifsearcher.viewmodel;
 import android.content.Context;
 import android.databinding.BaseObservable;
 import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.victorskurchik.gifsearcher.model.GifResult;
-import com.victorskurchik.gifsearcher.model.GifUrlSet;
 import com.victorskurchik.gifsearcher.view.GifDetailActivity;
 
 public class ItemGifViewModel extends BaseObservable {
@@ -25,17 +31,25 @@ public class ItemGifViewModel extends BaseObservable {
         return gifResult.id;
     }
 
-    public GifUrlSet getImages() {
-        return gifResult.images;
-    }
-
     public String getImageUrl() {
         return gifResult.images.original.url;
     }
 
-    @BindingAdapter("imageUrl")
-    public static void setImageUrl(ImageView imageView, String url) {
-        Glide.with(imageView.getContext()).load(url).into(imageView);
+    @BindingAdapter({"imageUrl", "progressbar"})
+    public static void setImageUrl(final ImageView imageView, String url, final ProgressBar progressBar) {
+        Glide.with(imageView.getContext()).load(url).listener(new RequestListener<Drawable>() {
+            @Override
+            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                progressBar.setVisibility(View.GONE);
+                return false;
+            }
+
+            @Override
+            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                progressBar.setVisibility(View.GONE);
+                return false;
+            }
+        }).into(imageView);
     }
 
     public void onItemClick(View view) {
